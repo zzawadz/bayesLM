a3ab89e8b62d223f4939be138db39b07c1384671
zzawadz <zawadzkizygmunt@gmail.com>
2014-02-04 15:00:38 +0100

    z
    z

--- R/getSummaryStatistics.R
+++ R/getSummaryStatistics.R
@@ -1,5 +1,4 @@
 
-
 setMethod("getSummaryStatistics", "BLM", function(model)
 {
   
@@ -7,19 +6,3 @@ setMethod("getSummaryStatistics", "BLM", function(model)
   cat(paste(t_n,round(model@coeff,4), sep = ":  "),sep="\n")
   
 })
-
-require(bayesLM)
-
-
-x = 1:100
-y = 5*x+6+rnorm(100)
-
-dane = data.frame(y,x)
-
-#Zaczynamy liczyc
-require(bayesLM, quietly=TRUE) #1
-model = fitBLM(y~x, dane)      #2
-plotCoefDistributions(model)   #3
-
-
-getSummaryStatistics(model)
